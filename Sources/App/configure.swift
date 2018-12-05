96074a5549e7c0f5e4bb454273066f464dafc824
lmbcosta <lmbcosta@hotmail.com>
2018-12-05 20:33:27 +0000

    Fixng PostgresSQL issues

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -30,16 +30,31 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 //    databases.add(database: sqlite, as: .sqlite)
 //    services.register(databases)
     var databases = DatabasesConfig()
+    
+    let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+    let username = Environment.get("DATABASE_USER") ?? "vapor"
+    let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+    let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+    
+    let databaseConfig = PostgreSQLDatabaseConfig(hostname: hostname, username: username, database: databaseName, password: password)
+    let database = PostgreSQLDatabase(config: databaseConfig)
+    
+    databases.add(database: database, as: .psql)
+    services.register(databases)
+    
+    
+    
+    
     //let databaseConfig = MySQLDatabaseConfig(hostname: "localhost", username: "vapor", password: "password", database: "vapor")
     //let database = MySQLDatabase(config: databaseConfig)
     //databases.add(database: database, as: .mysql)
-    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
-                                                  username: "vapor",
-                                                  database: "vapor",
-                                                  password: "password")
-    let database = PostgreSQLDatabase(config: databaseConfig)
-    databases.add(database: database, as: .psql)
-    services.register(databases)
+//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
+//                                                  username: "vapor",
+//                                                  database: "vapor",
+//                                                  password: "password")
+//    let database = PostgreSQLDatabase(config: databaseConfig)
+//    databases.add(database: database, as: .psql)
+//    services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
