4889fc73b6a3b639fb8a01f91dfd07eb7d21dd98
lmbcosta <lmbcosta@hotmail.com>
2018-12-22 16:27:33 +0000

    Add CRUD operations

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -17,20 +17,10 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
-    /// middlewares.use(FileMiddleware.self) // Serves files from `Public/` directory
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    //let sqlite = try SQLiteDatabase(storage: .memory)
-    //let sqlite = try SQLiteDatabase(storage: .file(path: "db.sqlite"))
-
-    /// Register the configured SQLite database to the database config.
-//    var databases = DatabasesConfig()
-//    databases.add(database: sqlite, as: .sqlite)
-//    services.register(databases)
     var databases = DatabasesConfig()
-    
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
@@ -41,20 +31,6 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     databases.add(database: database, as: .psql)
     services.register(databases)
-    
-    
-    
-    
-    //let databaseConfig = MySQLDatabaseConfig(hostname: "localhost", username: "vapor", password: "password", database: "vapor")
-    //let database = MySQLDatabase(config: databaseConfig)
-    //databases.add(database: database, as: .mysql)
-//    let databaseConfig = PostgreSQLDatabaseConfig(hostname: "localhost",
-//                                                  username: "vapor",
-//                                                  database: "vapor",
-//                                                  password: "password")
-//    let database = PostgreSQLDatabase(config: databaseConfig)
-//    databases.add(database: database, as: .psql)
-//    services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
